c588cca76ca5d786b789ca5b09d7935adc1e0e71
GoldenVapour <markwilcock@live.com>
2013-08-10 18:36:14 +0100

    Added basic skeleton

--- Pandemic/src/co/blazemc/pandemic/Pandemic.java
+++ Pandemic/src/co/blazemc/pandemic/Pandemic.java
@@ -1,5 +1,17 @@
 package co.blazemc.pandemic;
 
-public class Pandemic {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Pandemic extends JavaPlugin {
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	@Override
+	public void onEnable(){
+				
+	}
 
 }
